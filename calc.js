49a4842bc1b4fb91f3642327900c099aea9cb932
softpaper95 <minho100227@gamil.com>
2022-03-02 23:16:13 +0900

    section6 쇠막대기

--- calc.js
+++ calc.js
@@ -1,7 +1,8 @@
-let x = "abc";
-let map = new Map();
-map.set("a", 3);
-map.set("b", 5);
-let num = map.values();
-console.log(num);
-if (num === 8) console.log("true");
+let a = "9";
+let b = "a";
+function isNumber(index) {
+  if (parseInt(index)) console.log(true);
+  else console.log(false);
+}
+isNumber(a);
+isNumber(b);
--- section6/카카오 크레인2.js
+++ section6/카카오 크레인2.js
@@ -1,3 +1,8 @@
+//loop로 moves의 요소(mov)들을 받아온다.
+// loop로 board[i][mov-1]에서 i를 돌려보며 0이 아닐때를 찾는다.
+// board[i][mov-1]를 stack에 넣어주고 0으로 바꿔준다.
+//  만약 stack의 마지막 요소가 board[i][mov-1]와 같다면 stack의 마지막요소를 제거해주고 answer+=2를 해준다.
+
 let board = [
   [0, 0, 0, 0, 0],
   [0, 0, 1, 0, 3],
@@ -6,11 +11,6 @@ let board = [
   [3, 5, 1, 3, 1],
 ];
 let moves = [1, 5, 3, 5, 1, 2, 1, 4];
-
-//loop로 moves의 요소(mov)들을 받아온다.
-// loop로 board[i][mov-1]에서 i를 돌려보며 0이 아닐때를 찾는다.
-// board[i][mov-1]를 stack에 넣어주고 0으로 바꿔준다.
-//  만약 stack의 마지막 요소가 board[i][mov-1]와 같다면 stack의 마지막요소를 제거해주고 answer+=2를 해준다.
 
 let answer = 0;
 let stack = [];
