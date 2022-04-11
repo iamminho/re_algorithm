d425de297bf9c37466bee0db611584763321c67c
iamminho <minho100227@kakao.com>
2022-04-11 11:59:09 +0900

    프로그래머스 스택,큐 기능개발

---
+++ 프로그래머스/스택,큐/기능개발.js
@@ -0,0 +1,38 @@
+//문제이해하기
+// progresses[0]이 완료되면 뒤에 완료된 작업들도 배포된다.
+//   이때 몇개가 return되는지 확인한다.
+// 완료되지 않은 작업들중 첫번재 순서가 progresses[0]으로 오며 
+// 다시 이 작업이 완료되면 뒤의 완료된 작업들과 함께 배포된다.
+
+//문제 해결전략
+// progresses[0]가 100이될때 완료된 작업들의 개수를 구한다.
+// 완료된 작업들은 progeresses에서 제거한다.
+
+function solution(progresses, speeds) {
+    let answer = [];
+    while(progresses.length>0) {
+        console.log("pro1", progresses);
+        let stack = [];
+        while (progresses[0] < 100) {
+            progresses.map((p, i) => (progresses[i] = p + speeds[i]));
+            console.log(progresses);
+          }
+        console.log("pro2",progresses);
+        for(let i=0; i<progresses.length; i++){
+            if(progresses[i] >= 100) stack.push(progresses[i]);
+            else break;
+        }
+        console.log("stack:", stack);
+        progresses = progresses.splice(stack.length);
+        speeds = speeds.splice(stack.length);
+        answer.push(stack.length);
+    }
+    return answer;
+}
+let progresses = [95,95,95,95];
+let speeds = [4,3,2,1];
+console.log(solution(progresses, speeds));
+
+
+      
+      
--- 프로그래머스/해쉬/베스트앨범.js
+++ 프로그래머스/해쉬/베스트앨범.js
@@ -9,34 +9,70 @@
 //      대소를 비교할 때 값이 같다면 고유번호가 작은것을 우선으로 할 것.
 
 
-
-
 function solution(genres, plays) {
-    let answer = [];
-    let obj = {};
-    for(let i = 0; i<genres.length; i++){
-        obj[genres[i]] = (obj[genres[i]] || 0) + plays[i] ;
-    }
-    let sort = Object.entries(obj).sort(([, a], [, b]) => b - a);
-    console.log(sort);
-    for(let s of sort) {
-        let tmp = [];
-        for(let idx = 0; idx<genres.length; idx++){
-            if(s[0] === genres[idx]){
-                tmp.push([idx,plays[idx]]);
-            }
-        }
-        tmp.sort(([,a],[,b]) => b-a);
-        if(tmp.length>1) answer.push(tmp[0][0], tmp[1][0]);
-        else answer.push(tmp[0][0]);
-                
-    }    
-    return answer;
+    var dic = {};
+    genres.forEach((t,i)=> {
+        dic[t] = dic[t] ?  dic[t] + plays[i] :plays[i];        
+    });
+
+    var dupDic = {};
+
+    var tmp = genres.map((t,i)=> ({genre : t, count:plays[i] , index:i}))
+    tmp.sort((a,b)=>{               
+        if(a.genre !== b.genre) return dic[b.genre] - dic[a.genre]; // 장르별로 모아모아
+        if(a.count !== b.count) return b.count - a.count; //count는 큰 순서대로
+        return a.index - b.index; // index는 적은순서대로
+    }).filter(t=>  {
+        if(dupDic[t.genre] >= 2) return false;                                               
+        dupDic[t.genre] = dupDic[t.genre] ? dupDic[t.genre]+ 1 : 1;               
+        return true;
+     })
+     console.log(tmp);
+    
+    return genres          
+          .map((t,i)=> ({genre : t, count:plays[i] , index:i}))
+          .sort((a,b)=>{               
+               if(a.genre !== b.genre) return dic[b.genre] - dic[a.genre]; // 장르는 dic의 value값이 큰 순서대로
+               if(a.count !== b.count) return b.count - a.count; //count는 큰 순서대로
+               return a.index - b.index; // index는 적은순서대로
+           })
+           .filter(t=>  {
+               if(dupDic[t.genre] >= 2) return false;                                               
+               dupDic[t.genre] = dupDic[t.genre] ? dupDic[t.genre]+ 1 : 1;               
+               return true;
+            })
+           .map(t=> t.index);    
 }
 
 let genres = ["classic", "pop", "classic", "classic", "pop"];
 let plays = [500, 600, 800, 800, 2500];
 console.log(solution(genres, plays))
+
+
+// function solution(genres, plays) {
+//     let answer = [];
+//     let obj = {};
+//     for(let i = 0; i<genres.length; i++){
+//         obj[genres[i]] = (obj[genres[i]] || 0) + plays[i] ;
+//     }
+//     let sort = Object.entries(obj).sort(([, a], [, b]) => b - a);
+//     console.log(sort);
+//     for(let s of sort) {
+//         let tmp = [];
+//         for(let idx = 0; idx<genres.length; idx++){
+//             if(s[0] === genres[idx]){
+//                 tmp.push([idx,plays[idx]]);
+//             }
+//         }
+//         tmp.sort(([,a],[,b]) => b-a);
+//         if(tmp.length>1) answer.push(tmp[0][0], tmp[1][0]);
+//         else answer.push(tmp[0][0]);
+                
+//     }    
+//     return answer;
+// }
+
+
 
 // function solution(genres, plays) {
 //     let answer = [];
