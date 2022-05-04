846b061d3f4fc56775994e6c2d30756d17b470b1
iamminho <minho100227@kakao.com>
2022-05-04 22:24:22 +0900

    순위검색 정확성은 통과 효율성 시간초과

--- 프로그래머스/순위검색.js
+++ 프로그래머스/순위검색.js
@@ -45,3 +45,49 @@ function solution(info, query) {
     // console.log(result);    
     return result;
 }
+
+
+
+//-----------------------------------------------------------------------
+
+
+// info에서 obj로 나눈다.
+// query는 ,를 기준으로 나눈다.
+// query는 " "를 기준으로 한번 더 나눈다. => 2중배열
+// query의 각 값들이 info와 같은지 확인한다.
+// 같다면 result의 알맞은 idx에 ++해준다.
+function solution(info, query) {  
+    let answer = []
+    info = info.map(v => v.split(","));
+    // console.log(info);
+    query = query.forEach(v => {
+        v = v.split(" ").filter(x => x !== 'and');  
+        // console.log("------------------------------------");
+        // console.log(v);
+        let stack = [];
+        let tmp = [...info];
+        for(let ele of v){  
+            // console.log("ele",ele);
+            if(!isNaN(ele)) {
+                // console.log("Number");
+                tmp = tmp.filter(arr => {
+                        let res = arr[0];
+                        let regex = /[^0-9]/g;
+                        let num = res.replace(regex, "");
+                        if(Number(ele) <= Number(num)){
+                            return arr[0]
+                        }                       
+                    })                                                                                   
+            }else if(ele === "-") continue;
+            else{
+                // console.log("String!");
+                tmp = tmp.filter(arr => {                                                              
+                    if(arr[0].includes(ele)) return arr[0];              
+                });
+                // console.log("tmp",tmp);
+            }                      
+        }
+        answer.push(tmp.length);     
+    });
+    return answer;
+}
