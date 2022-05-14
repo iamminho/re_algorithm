0e93e1d1315a63ab6e355de9fcbe09b7fc1e4be9
iamminho <minho100227@kakao.com>
2022-05-14 22:18:16 +0900

    프로그래머스 구명보트 시간초과

--- 프로그래머스/구명보트.js
+++ 프로그래머스/구명보트.js
@@ -15,3 +15,27 @@ function solution(people, limit) {
   }, 0);
   return answer + 1;
 }
+
+// 시간초과
+function solution(people, limit) {
+  var answer = 0;
+  people = people.sort((a, b) => a - b);
+  function search(pop, arr) {
+    let sum = pop;
+    let stack = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+      if (sum + arr[i] <= limit) {
+        stack.push(i);
+        sum += arr[i];
+      }
+    }
+    arr.splice(stack[stack.length - 1], stack.length);
+    answer++;
+    return (people = arr);
+  }
+  while (people.length > 0) {
+    let pop = people.shift();
+    search(pop, people);
+  }
+  return answer;
+}
