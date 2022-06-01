a14954a47e7f54a0fd60522d69204aa00d299c8f
iamminho <minho100227@kakao.com>
2022-06-01 23:53:42 +0900

    캐시 v.2

--- 프로그래머스/캐시.js
+++ 프로그래머스/캐시.js
@@ -1,3 +1,33 @@
+//v.2
+function solution(cacheSize, cities) {
+  let cache = [];
+  let time = 0;
+
+  if (!cacheSize) return cities.length * 5;
+  function move_latest(city) {
+    let idx = cache.indexOf(city);
+    cache.push(...cache.splice(idx, 1));
+  }
+
+  cities.forEach((city) => {
+    city = city.toUpperCase();
+    // cache hit
+    if (cache.includes(city)) {
+      let idx = cache.indexOf(city);
+      cache.splice(idx, 1);
+      time++;
+      // cache miss
+    } else {
+      if (cache.length === cacheSize) cache.shift();
+      time += 5;
+    }
+    cache.push(city);
+    // console.log("cache, time", cache, time);
+  });
+  return time;
+}
+
+//v.1
 function solution(cacheSize, cities) {
   let cache = [];
   let time = 0;
