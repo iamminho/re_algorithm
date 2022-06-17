21e2c8769b616746a49f389247ab15c897e039e7
iamminho <minho100227@kakao.com>
2022-06-17 10:57:46 +0900

    section10 돌다리건너기

---
+++ section10/돌다리 건너기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  let dy = Array.from({ length: n + 2 }, () => 0);
+  dy[1] = 1;
+  dy[2] = 2;
+  for (let i = 3; i <= n + 1; i++) {
+    dy[i] = dy[i - 2] + dy[i - 1];
+  }
+  return dy[dy.length - 1];
+}
+let n = 7;
+console.log(solution(n));
