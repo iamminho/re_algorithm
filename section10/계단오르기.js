cb65d69e53c6ba44bd8ff1271ec4bf4aa074500f
iamminho <minho100227@kakao.com>
2022-06-17 10:40:53 +0900

    section10 계단오르기

--- section10/계단오르기.js
+++ section10/계단오르기.js
@@ -2,7 +2,6 @@ function solution(n) {
   let dy = Array.from({ length: n + 1 }, () => 0);
   dy[1] = 1;
   dy[2] = 2;
-  let i = 3;
   for (let i = 3; i <= n; i++) {
     dy[i] = dy[i - 2] + dy[i - 1];
   }
