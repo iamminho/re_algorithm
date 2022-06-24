2f645065ad0aef0b60f293ae265714ee0568ede2
iamminho <minho100227@kakao.com>
2022-06-24 22:05:24 +0900

    베스트앨범

--- 프로그래머스/해쉬/베스트앨범.js
+++ 프로그래머스/해쉬/베스트앨범.js
@@ -7,31 +7,32 @@
 // 1. 장르별 합을 구할것
 // 2. 합이 큰 장르부터 그안의 대소를 비교하여 2개를 answer로 push할 것.
 //      대소를 비교할 때 값이 같다면 고유번호가 작은것을 우선으로 할 것.
-function solution(genres, plays) {
-  let answer = [];
-  let new_arr = [];
-  for (let i = 0; i < genres.length; i++) {
-    new_arr.push([genres[i], plays[i], i]);
-  }
-  new_arr.sort((a, b) => {
-    if (a[1] > b[1]) return -1;
-  });
-  let obj = new Object();
-  for (let arr of new_arr) {
-    if (obj[arr[0]]) {
-      obj[arr[0]][0] += arr[1];
-      if (obj[arr[0]][1].length < 2)
-        obj[arr[0]][1] = [...obj[arr[0]][1], arr[2]];
-    } else obj[arr[0]] = [arr[1], [arr[2]]];
-  }
-  let result = Object.values(obj).sort((a, b) => {
-    if (a[0] > b[0]) return -1;
-  });
-  result.forEach((arr) => {
-    answer.push(...arr[1]);
-  });
-  return answer;
-}
+
+// function solution(genres, plays) {
+//   let answer = [];
+//   let new_arr = [];
+//   for (let i = 0; i < genres.length; i++) {
+//     new_arr.push([genres[i], plays[i], i]);
+//   }
+//   new_arr.sort((a, b) => {
+//     if (a[1] > b[1]) return -1;
+//   });
+//   let obj = new Object();
+//   for (let arr of new_arr) {
+//     if (obj[arr[0]]) {
+//       obj[arr[0]][0] += arr[1];
+//       if (obj[arr[0]][1].length < 2)
+//         obj[arr[0]][1] = [...obj[arr[0]][1], arr[2]];
+//     } else obj[arr[0]] = [arr[1], [arr[2]]];
+//   }
+//   let result = Object.values(obj).sort((a, b) => {
+//     if (a[0] > b[0]) return -1;
+//   });
+//   result.forEach((arr) => {
+//     answer.push(...arr[1]);
+//   });
+//   return answer;
+// }
 
 function solution(genres, plays) {
   var dic = {};
@@ -40,27 +41,12 @@ function solution(genres, plays) {
   });
 
   var dupDic = {};
-
-  var tmp = genres.map((t, i) => ({ genre: t, count: plays[i], index: i }));
-  tmp
-    .sort((a, b) => {
-      if (a.genre !== b.genre) return dic[b.genre] - dic[a.genre]; // 장르별로 모아모아
-      if (a.count !== b.count) return b.count - a.count; //count는 큰 순서대로
-      return a.index - b.index; // index는 적은순서대로
-    })
-    .filter((t) => {
-      if (dupDic[t.genre] >= 2) return false;
-      dupDic[t.genre] = dupDic[t.genre] ? dupDic[t.genre] + 1 : 1;
-      return true;
-    });
-  console.log(tmp);
-
-  return genres
+  let result = genres
     .map((t, i) => ({ genre: t, count: plays[i], index: i }))
     .sort((a, b) => {
-      if (a.genre !== b.genre) return dic[b.genre] - dic[a.genre]; // 장르는 dic의 value값이 큰 순서대로
-      if (a.count !== b.count) return b.count - a.count; //count는 큰 순서대로
-      return a.index - b.index; // index는 적은순서대로
+      if (a.genre !== b.genre) return dic[b.genre] - dic[a.genre];
+      if (a.count !== b.count) return b.count - a.count;
+      return a.index - b.index;
     })
     .filter((t) => {
       if (dupDic[t.genre] >= 2) return false;
@@ -68,6 +54,7 @@ function solution(genres, plays) {
       return true;
     })
     .map((t) => t.index);
+  return result;
 }
 
 let genres = ["classic", "pop", "classic", "classic", "pop"];
