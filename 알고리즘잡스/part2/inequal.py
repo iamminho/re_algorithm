e762ea3a29a85239a2a898ba55084e0cbc13bd77
iamminho <minho100227@kakao.com>
2022-08-04 16:18:06 +0900

    알고리즘잡스 part2 inequal

---
+++ 알고리즘잡스/part2/inequal.py
@@ -0,0 +1,36 @@
+k = 2
+arr = ['<', '>']
+ch = [True] * 10
+res = [0] * (k+1)
+answers = []
+
+
+def check(sign, num1, num2):
+    if sign == '<':
+        if num1 < num2:
+            return True
+        else:
+            return False
+    else:
+        if num1 > num2:
+            return True
+        else:
+            return False
+
+
+def getNumber(L):
+    if L == k+1:
+        answers.append(''.join(map(str, res)))
+    else:
+        for i in range(0, 10):
+            if ch[i] == True:
+                if L == 0 or check(arr[L-1], res[L-1], i):
+                    ch[i] = False
+                    res[L] = i
+                    getNumber(L+1)
+                    ch[i] = True
+
+
+getNumber(0)
+print(answers[-1])
+print(answers[0])
